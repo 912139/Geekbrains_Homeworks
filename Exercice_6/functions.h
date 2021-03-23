c0b5e2397b557a83ff2bf75433afaf4ff66486af
912139 <78167919+912139@users.noreply.github.com>
2021-03-23 11:40:28 +0300

    Add files via upload

---
+++ Exercice_6/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+namespace functions {
+int change (int *arr);
+int change1 (int*arr);
+bool function1 (int*arr);
+int function2 (int*arr, int n);
+void change2 (int n, ...);
+}
+#endif // FUNCTIONS_H_INCLUDED
+
